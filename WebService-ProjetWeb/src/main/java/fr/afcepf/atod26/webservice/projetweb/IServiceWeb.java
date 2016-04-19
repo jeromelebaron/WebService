f70c41ef75be4b9d21ef08a8921fecc1253a1287
Jerdopler <lebaronjerome@free.fr>
2016-04-19 16:14:53 +0200

    Ajout des log

--- WebService-ProjetWeb/src/main/java/fr/afcepf/atod26/webservice/projetweb/IServiceWeb.java
+++ WebService-ProjetWeb/src/main/java/fr/afcepf/atod26/webservice/projetweb/IServiceWeb.java
@@ -8,7 +8,7 @@ import javax.jws.soap.SOAPBinding;
 import javax.jws.soap.SOAPBinding.Style;
 
 @WebService(targetNamespace = "http://lebaronjerome.atod26.afcepf.fr")
-@SOAPBinding(style = Style.RPC)
+@SOAPBinding(style = Style.DOCUMENT)
 public interface IServiceWeb {
 
 	@WebMethod(operationName = "ajouter")
