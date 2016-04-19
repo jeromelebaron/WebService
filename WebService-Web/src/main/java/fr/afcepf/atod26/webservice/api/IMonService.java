ff514c57e17ed1afc60dfdbc0df578f1c7b60ecb
Jerdopler <lebaronjerome@free.fr>
2016-04-19 10:23:45 +0200

    Ajout du projet web Ajout de l'interface et de l'implémentation du premier WebService

---
+++ WebService-Web/src/main/java/fr/afcepf/atod26/webservice/api/IMonService.java
@@ -0,0 +1,9 @@
+package fr.afcepf.atod26.webservice.api;
+
+public interface IMonService {
+
+	int add(final int premier, final int deuxieme);
+
+	double puissance(final int premier, final int deuxieme);
+
+}
---
+++ WebService-Web/src/main/java/fr/afcepf/atod26/webservice/impl/MonServiceimpl.java
@@ -0,0 +1,23 @@
+package fr.afcepf.atod26.webservice.impl;
+
+import org.apache.log4j.Logger;
+
+import fr.afcepf.atod26.webservice.api.IMonService;
+
+public class MonServiceimpl implements IMonService {
+
+	private Logger log = Logger.getLogger(MonServiceimpl.class);
+
+	@Override
+	public int add(final int premier, final int deuxieme) {
+		log.debug("Methode ajout");
+		return premier + deuxieme;
+	}
+
+	@Override
+	public double puissance(final int premier, final int deuxieme) {
+		log.debug("Methode puissance");
+		return premier * deuxieme;
+	}
+
+}
